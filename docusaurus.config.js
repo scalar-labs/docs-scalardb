d35715185eaa042cb22bdb5bbb33702ca945acca
josh-wong <23216828+josh-wong@users.noreply.github.com>
2024-04-22 12:37:48 +0900

    Add Docusaurus file

---
+++ docusaurus.config.js
@@ -0,0 +1,327 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'ScalarDB Documentation',
+  tagline: 'Cloud-native universal transaction manager',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://scalardb.scalar-labs.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'scalar-labs', // Usually your GitHub org/user name.
+  projectName: 'docs-scalardb', // Usually your repo name.
+
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en-us',
+    locales: ['en-us', 'ja-jp'],
+    localeConfigs: {
+      'en-us': {
+        label: 'English',
+        direction: 'ltr',
+        htmlLang: 'en-US',
+        calendar: 'gregory',
+        path: 'versioned_docs/en-us',
+      },
+      'ja-jp': {
+        label: '日本語',
+        direction: 'ltr',
+        htmlLang: 'ja-JP',
+        calendar: 'gregory',
+        path: 'versioned_docs/ja-jp',
+      },
+    },
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          breadcrumbs: true,
+          sidebarPath: './sidebars.js',
+          // Remove this to remove the "edit this page" links.
+          editUrl: 'https://github.com/scalar-labs/docs-scalardb/edit/main',
+          editLocalizedFiles: true, // This allows for the "Edit this page" button to direct users to edit the localized version of the page on GitHub.
+          lastVersion: 'current',
+            versions: {
+              // The following is a template for adding a new version to the dropdown menu. Copy this version template when adding a new version to the dropdown menu but don't delete it.
+              /*
+              current: {
+                label: '<VERSION_NUMBER>',
+                path: 'latest',
+                banner: 'none',
+              },
+              */
+              current: { // Change this to the version number when a new version is released.
+                label: '3.12',
+                path: 'latest', // Change this to the version number when a new version is released.
+                banner: 'none',
+              },
+              3.11: { // Change this to the version number when a new version is released.
+                label: '3.11',
+                path: '3.11', // Change this to the version number when a new version is released.
+                banner: 'none',
+              },
+              3.10: { // Change this to the version number when a new version is released.
+                label: '3.10',
+                path: '3.10', // Change this to the version number when a new version is released.
+                banner: 'none',
+              },
+              3.9: {
+                label: '3.9',
+                path: '3.9',
+                banner: 'none',
+              },
+              3.8: { // Change this to the version number when a new version is released.
+                label: '3.8',
+                path: '3.8', // Change this to the version number when a new version is released.
+                banner: 'none',
+              },
+              3.7: {
+                label: '3.7',
+                path: '3.7',
+                banner: 'none',
+              },
+              3.6: {
+                label: '3.6 (unsupported)',
+                path: '3.6',
+                banner: 'unmaintained',
+              },
+              3.5: {
+                label: '3.5 (unsupported)',
+                path: '3.5',
+                banner: 'unmaintained',
+              },
+              3.4: {
+                label: '3.4 (unsupported)',
+                path: '3.4',
+                banner: 'unmaintained',
+              },
+            },
+          },
+          googleTagManager: {
+            containerId: 'GTM-WL9C9L5',
+          },
+          gtag: {
+            trackingID: 'G-Q4TKS77KCP',
+            anonymizeIP: true,
+          },
+        // Maybe we can use this later.
+        /*
+        blog: {
+          showReadingTime: true,
+        },
+        */
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  plugins: [
+    [
+      '@docusaurus/plugin-client-redirects',
+      {
+        redirects: [
+          // This redirect takes the user to the latest version of the English docs when they land on the English versions of the docs site.
+          {
+            to: '/docs/latest',
+            from: ['/', '/docs'],
+          },
+          {
+            to: '/docs/latest/releases/release-support-policy',
+            from: '/docs/releases/release-support-policy',
+          },
+        ],
+        createRedirects(existingPath) {
+          if (existingPath.includes('/ja-jp/docs')) {
+            // Redirect from /docs/ja-jp/X to /ja-jp/docs/X.
+            return [
+              existingPath.replace('/ja-jp/docs', '/docs/ja-jp'),
+            ];
+          }
+          return undefined; // Return a falsy value: no redirect created
+        },
+      },
+    ],
+    // [
+    //   require.resolve("docusaurus-lunr-search"),
+    //   {
+    //     enableHighlight: true,
+    //     languages: ['en', 'ja'], // language codes
+    //     includeRoutes: ['/docs/latest/**', '/ja-jp/docs/latest/**'],
+    //   },
+    // ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/scalardb-logo.png',
+      navbar: {
+        title: '',
+        logo: {
+          alt: 'ScalarDB logo',
+          src: 'img/scalardb-logo.png',
+          href: 'https://scalardb.scalar-labs.com/docs/',
+          target: '_self',
+        },
+        items: [
+          {
+            type: 'docsVersionDropdown',
+            position: 'left',
+            dropdownActiveClassDisabled: true,
+          },
+          /*
+          The "localeDropdown" configuration should be uncommented (enabled) after we create Japanese versions of docs. 
+          */
+          // {
+          //   type: 'localeDropdown',
+          //   position: 'right',
+          // },
+        ],
+      },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: '3M5M8MK1GK',
+
+        // Public API key: it is safe to commit it
+        apiKey: '1b88519fe6902d57179f5922b27b8c7a',
+
+        indexName: 'scalardb-scalar-labs',
+
+        // Optional: see doc section below
+        contextualSearch: true,
+
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        // externalUrlRegex: 'external\\.com|domain\\.com',
+
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        // replaceSearchResultPathname: {
+        //   from: '/docs/', // or as RegExp: /\/docs\//
+        //   to: '/',
+        // },
+
+        // Optional: Algolia search parameters
+        // searchParameters: {},
+
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+
+        //... other Algolia params
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Products',
+            items: [
+              {
+                label: 'ScalarDB',
+                href: 'https://www.scalar-labs.com/scalardb',
+              },
+              {
+                label: 'ScalarDL',
+                href: 'https://www.scalar-labs.com/scalardl',
+              },
+            ],
+          },
+          {
+            title: 'Company',
+            items: [
+              {
+                label: 'About us',
+                href: 'https://www.scalar-labs.com/about-us',
+              },
+              {
+                label: 'News',
+                href: 'https://www.scalar-labs.com/news',
+              },
+              {
+                label: 'Scalar Engineering Blog',
+                href: 'https://medium.com/scalar-engineering',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/scalar-labs',
+              },
+              {
+                label: 'LinkedIn',
+                href: 'https://www.linkedin.com/company/scalarlabs',
+              },
+              {
+                label: 'YouTube',
+                href: 'https://www.youtube.com/@scalar-labs',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/scalar_labs',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Pricing',
+                href: 'https://www.scalar-labs.com/pricing',
+              },
+              {
+                label: 'Docs',
+                href: 'https://developers.scalar-labs.com/docs',
+              },
+              {
+                label: 'Support',
+                href: 'https://www.scalar-labs.com/support',
+              },
+              {
+                label: 'Contact us',
+                href: 'https://www.scalar-labs.com/contact',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Scalar, Inc.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+      // announcementBar: {
+      //   id: 'new_version',
+      //   content:
+      //     '<b>ScalarDB X.X is now available!🥳 For details on what\'s included in this new version, see the <a target="_self" rel="noopener noreferrer" href="/docs/latest/releases/release-notes">release notes</a>.<b>',
+      //   backgroundColor: '#2673BB',
+      //   textColor: '#FFFFFF',
+      //   isCloseable: false,
+      // },
+    }),
+};
+
+export default config;
