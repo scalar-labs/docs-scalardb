1812c977bc1072a7fbee74dd987faf057365bfbb
josh-wong <23216828+josh-wong@users.noreply.github.com>
2024-05-07 17:45:39 +0900

    Change link syntax; fix broken links

--- src/components/Cards/index.tsx
+++ src/components/Cards/index.tsx
@@ -17,7 +17,7 @@ const CardsAbout = [
     // name: '',
     // image: '<LINK_TO>.png',
     url: {
-      page: 'overview',
+      page: 'overview/',
     },
     description: (
       <Translate id="home.about.description">
@@ -29,7 +29,7 @@ const CardsAbout = [
     // name: '',
     // image: '<LINK_TO>.png',
     url: {
-      page: 'scalardb-supported-databases',
+      page: 'scalardb-supported-databases/',
     },
     description: (
       <Translate id="home.about.description">
@@ -44,7 +44,7 @@ const CardsGettingStarted = [
     // name: '',
     // image: '<LINK_TO>.png',
     url: {
-      page: 'getting-started-with-scalardb',
+      page: 'getting-started-with-scalardb/',
     },
     description: (
       <Translate id="home.gettingStarted.description">
@@ -56,7 +56,7 @@ const CardsGettingStarted = [
     // name: '',
     // image: '<LINK_TO>.png',
     url: {
-      page: 'scalardb-cluster/getting-started-with-scalardb-cluster',
+      page: 'scalardb-cluster/getting-started-with-scalardb-cluster/',
     },
     description: (
       <Translate id="home.gettingStarted.description">
@@ -71,7 +71,7 @@ const CardsSamples = [
     // name: '',
     // image: '<LINK_TO>.png',
     url: {
-      page: 'scalardb-samples/scalardb-sample/README',
+      page: 'scalardb-samples/scalardb-sample/',
     },
     description: (
       <Translate id="home.samples.description">
@@ -83,7 +83,7 @@ const CardsSamples = [
     // name: '',
     // image: '<LINK_TO>.png',
     url: {
-      page: 'scalardb-samples/multi-storage-transaction-sample/README',
+      page: 'scalardb-samples/multi-storage-transaction-sample/',
     },
     description: (
       <Translate id="home.samples.description">
@@ -98,7 +98,7 @@ const CardsDevelop = [
     // name: 'For database engineers',
     // image: '<LINK_TO>.png',
     url: {
-      page: 'add-scalardb-to-your-build',
+      page: 'add-scalardb-to-your-build/',
     },
     description: (
       <Translate id="home.develop.description">
@@ -110,7 +110,7 @@ const CardsDevelop = [
     // name: 'For infrastructure engineers',
     // image: '<LINK_TO>.png',
     url: {
-      page: 'schema-loader',
+      page: 'schema-loader/',
     },
     description: (
       <Translate id="home.develop.description">
@@ -125,7 +125,7 @@ const CardsDeploy = [
     // name: '',
     // image: '<LINK_TO>.png',
     url: {
-      page: 'scalar-kubernetes/SetupDatabaseForAWS',
+      page: 'scalar-kubernetes/SetupDatabaseForAWS/',
     },
     description: (
       <Translate id="home.deploy.description">
@@ -137,7 +137,7 @@ const CardsDeploy = [
     // name: '',
     // image: '<LINK_TO>.png',
     url: {
-      page: 'scalar-kubernetes/ProductionChecklistForScalarDBCluster',
+      page: 'scalar-kubernetes/ProductionChecklistForScalarDBCluster/',
     },
     description: (
       <Translate id="home.deploy.description">
@@ -152,7 +152,7 @@ const CardsManage = [
     // name: '',
     // image: '<LINK_TO>.png',
     url: {
-      page: 'scalar-kubernetes/K8sMonitorGuide',
+      page: 'scalar-kubernetes/K8sMonitorGuide/',
     },
     description: (
       <Translate id="home.manage.description">
@@ -164,7 +164,7 @@ const CardsManage = [
     // name: '',
     // image: '<LINK_TO>.png',
     url: {
-      page: 'scalar-kubernetes/BackupRestoreGuide',
+      page: 'scalar-kubernetes/BackupRestoreGuide/',
     },
     description: (
       <Translate id="home.manage.description">
@@ -179,7 +179,7 @@ const CardsReference = [
     // name: '',
     // image: '<LINK_TO>.png',
     url: {
-      page: 'scalardb-benchmarks/README',
+      page: 'scalardb-benchmarks/',
     },
     description: (
       <Translate id="home.reference.description">
@@ -191,7 +191,7 @@ const CardsReference = [
     // name: '',
     // image: '<LINK_TO>.png',
     url: {
-      page: 'storage-abstraction',
+      page: 'storage-abstraction/',
     },
     description: (
       <Translate id="home.reference.description">
