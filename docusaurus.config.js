5726a233622194cc1f78b2a1e4f9e14b850b6bf6
josh-wong <23216828+josh-wong@users.noreply.github.com>
2024-07-03 20:32:23 +0900

    Make commit to force test Docusaurus deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,4 +1,4 @@
-// @ts-check
+// @ts-check 
 // `@type` JSDoc annotations allow editor autocompletion and type checking
 // (when paired with `@ts-check`).
 // There are various equivalent ways to declare your Docusaurus config.
