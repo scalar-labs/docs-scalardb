b7c0bf4e5d29db71b19dee1626e8c311913b8eb1
Josh Wong <23216828+josh-wong@users.noreply.github.com>
2025-06-11 17:19:53 +0900

    Revert "Add links to category labels"
    This reverts commit 7c4cf99a4b59e4a4861b787829d1659f580aedcb.

--- src/components/Cards/3.15.tsx
+++ src/components/Cards/3.15.tsx
@@ -24,15 +24,6 @@ const new_docs = [
   },
 ];
 
-const categoryLinks = {
-  'About ScalarDB': 'overview',
-  'Quickstart': 'quickstart-overview',
-  'Develop': 'develop-overview',
-  'Deploy': 'deploy-overview',
-  'Migrate': 'migrate-overview',
-  'Manage': 'manage-overview',
-};
-
 const categories = [
   {
     name: 'About ScalarDB',
@@ -107,11 +98,7 @@ const CategoryGrid = () => {
       {/* Category table */}
         {categories.map((cat, i) => (
           <React.Fragment key={i}>
-            <div className="category-label">
-              <Link to={categoryLinks[cat.name]} className="category-label-link">
-                {cat.name}
-              </Link>
-            </div>
+            <div className="category-label">{cat.name}</div>
             {cat.links.map((link, j) => (
               <Link key={j} className="category-cell" to={link}>
                 {cat.labels[j]}
