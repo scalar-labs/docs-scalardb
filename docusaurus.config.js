eab8fbc27e98615858e4de2b31fb91e50092f2ef
Josh Wong <joshua.wong@scalar-labs.com>
2025-06-24 16:19:44 +0900

    Update `className` for `3.15` to `3.15.4` in Docusaurus config file; add patch version release notes for `3.15.4` (#1328)
    * Update patch version for 3.15
    
    * Add release notes for 3.15.4
    
    * Update patch version for message about release notes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -78,7 +78,7 @@ const config = {
                 label: '3.15',
                 path: 'latest', // When a new version is released and this is no longer the current version, change this to the version number and then delete this comment.
                 banner: 'none',
-                className: '3.15.3',
+                className: '3.15.4',
               },
               "3.14": { // When a new version is released and this is no longer the current version, change this to the version number and then delete this comment.
                 label: '3.14',
