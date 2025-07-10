bfb7d8ae1c00538815a78a668c64b30d356d0654
Josh Wong <23216828+josh-wong@users.noreply.github.com>
2025-07-10 12:01:16 +0900

    Specify Db2 in link

--- src/data/notifications.js
+++ src/data/notifications.js
@@ -18,8 +18,8 @@ const notificationsList = [
       ja: 'ScalarDB 3.16 で IBM Db2 がサポートされました'
     },
     url: {
-      en: 'requirements#relational-databases?utm_source=docs-site&utm_medium=notifications',
-      ja: 'requirements#リレーショナルデータベース?utm_source=docs-site&utm_medium=notifications'
+      en: 'requirements?RDBs=Db2#relational-databases?utm_source=docs-site&utm_medium=notifications',
+      ja: 'requirements?RDBs=Db2#リレーショナルデータベース?utm_source=docs-site&utm_medium=notifications'
     },
     unread: true
   },
