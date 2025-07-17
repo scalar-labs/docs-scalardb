b04b8330db664422f50751ee7f1e28b707815c69
Josh Wong <23216828+josh-wong@users.noreply.github.com>
2025-07-17 16:56:08 +0900

    Revise comment about links in the `Recent features` section

--- src/components/Cards/ja-jp/3.13.tsx
+++ src/components/Cards/ja-jp/3.13.tsx
@@ -20,8 +20,8 @@ const recentFeatures = [
   {
     name: '新機能',
     categoryLinks: [
-      // To add a link, use the format ['link1', 'link2']
-      // To add a label, use the format ['label1', 'label2']
+      // To add a link, use the format ['link1', 'link2']. Links should be relative to the docs directory and not be anchor links.
+      // To add a label, use the format ['label1', 'label2'].
       {
         cell: 0, // First cell
         links: ['run-non-transactional-storage-operations-through-library#configure-scalardb-to-run-non-transactional-storage-operations'],
