e8000fcea5b8fd0481422cae856b2eb8444ed266
Josh Wong <23216828+josh-wong@users.noreply.github.com>
2025-08-18 12:57:18 +0900

    Revert "Add version number for clarity"
    This reverts commit 502ec80c298aedc904785b552743a679542c1a77.

--- src/data/notifications.js
+++ src/data/notifications.js
@@ -36,8 +36,8 @@ const notificationsList = [
   },
   {
     message: {
-      en: 'Discover how to implement vector search capabilities with ScalarDB Cluster 3.15',
-      ja: 'ScalarDB Cluster 3.15 でベクトル検索機能を実装する方法を学ぶ'
+      en: 'Discover how to implement vector search capabilities with ScalarDB Cluster',
+      ja: 'ScalarDB Cluster でベクトル検索機能を実装する方法を学ぶ'
     },
     url: {
       en: 'scalardb-cluster/getting-started-with-vector-search?utm_source=docs-site&utm_medium=notifications',
