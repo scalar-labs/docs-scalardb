a233af97afe1ed072cba20f5a74b98f469dff1ac
Josh Wong <23216828+josh-wong@users.noreply.github.com>
2026-04-30 15:40:41 +0900

    Update version in announcement from 3.17 to 3.18

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -507,7 +507,7 @@ const config = {
       announcementBar: {
         id: 'new_version',
         content:
-          'Announcing the release of ScalarDB 3.17!🚀 For details on what\'s included in this new version, see the <a target="_self" href="/docs/latest/releases/release-notes?utm_source=docs-site&utm_medium=announcementbar">release notes</a>.',
+          'Announcing the release of ScalarDB 3.18!🚀 For details on what\'s included in this new version, see the <a target="_self" href="/docs/latest/releases/release-notes?utm_source=docs-site&utm_medium=announcementbar">release notes</a>.',
           // 'Announcing the release of ScalarDB X.X!🚀 For details on what\'s included in this new version, see the <a target="_self" href="/docs/latest/releases/release-notes?utm_source=docs-site&utm_medium=announcementbar">release notes</a>',
         backgroundColor: '#2673BB',
         textColor: '#FFFFFF',
